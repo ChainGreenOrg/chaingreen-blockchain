afa5e9e95294866929d524c417edccce67bb752e
Almog De Paz <almogdepaz@gmail.com>
2021-06-15 17:50:41 +0300

    Validate summaries while syncing (#5284)
    * block pre process check se matches wp
    
    * test
    
    * test
    
    * test
    
    * lint
    
    * merge
    
    * lint
    # Conflicts:
    #	chaingreen/consensus/multiprocess_validation.py
    #	tests/core/full_node/full_sync/test_full_sync.py

--- chaingreen/consensus/multiprocess_validation.py
+++ chaingreen/consensus/multiprocess_validation.py
@@ -132,6 +132,7 @@ async def pre_validate_blocks_multiprocessing(
     npc_results: Dict[uint32, NPCResult],
     get_block_generator: Optional[Callable],
     batch_size: int,
+    wp_summaries: Optional[List[SubEpochSummary]] = None,
 ) -> Optional[List[PreValidationResult]]:
     """
     This method must be called under the blockchain lock
@@ -221,6 +222,13 @@ async def pre_validate_blocks_multiprocessing(
             block,
             None,
         )
+
+        if block_rec.sub_epoch_summary_included is not None and wp_summaries is not None:
+            idx = int(block.height / constants.SUB_EPOCH_BLOCKS) - 1
+            next_ses = wp_summaries[idx]
+            if not block_rec.sub_epoch_summary_included.get_hash() == next_ses.get_hash():
+                log.error("sub_epoch_summary does not match wp sub_epoch_summary list")
+                return None
         # Makes sure to not override the valid blocks already in block_records
         if not block_records.contains_block(block_rec.header_hash):
             block_records.add_block_record(block_rec)  # Temporarily add block to dict
