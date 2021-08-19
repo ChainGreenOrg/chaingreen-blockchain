Chia-Network/chia-blockchain

chia|tests: Introduce `PlotManager` + some plot loading improvements and fixes  (#7848)
* harvester|plotting|tests: Introduce `PlotManager` class

This moves all plots related and plot-directory related stuff from the
harvester into the class `PlotManager`, adjusts all related code
accordingly and adds some extra wrappers there.

* harvester|plotting|tests: Return how many new plots were loaded

* plotting: Fix `failed_to_open_filenames` re-try interval

With `< 1200` it just tries it on the next refresh.

* plotting: Fix and improve duplicates handling

* harvester|plotting: Thread locks for `PlotManager.plots`

* chia|tests: Load plots in batches

* chia|tests: Move plot refreshing into a separate thread

* plotting: Properly handle removed plots

And fix tests accordingly. It seems like this fix https://github.com/Chia-Network/chia-blockchain/pull/3350 wasn't really a fix, rather adjusting to allow for a bug?

* plotting|harvester|tests: Introduce `PlotRefreshResult`

* tests: Expand `test_farmer_harvester_rpc.py`

* chia|tests: Move some stuff from `plot_tools.py` into new file `util.py`

* refactor: Rename `plot_tools.py` to `manager.py`

* chia|tests: Use pure dataclass for `PlotsRefreshParameter`

With `uint16` as type saving to config doesn't work, this is a
preparation for the next commit.

* harvester: Adjust deprecation message, use `info` instead of `warning`

* plotting: Fix typo

* refactor: Rename `filename` to `file_path`

Fits better and does avoid shadowing with filename from outer scope.

* chia|tests: Move some methods from `plotting.manager` to `plotting.util`

* plotting: Make `refresh_callback` mandatory

chia/harvester/harvester_api.py
@@ -7,7 +7,7 @@ from blspy import AugSchemeMPL, G2Element, G1Element
 
 from chia.consensus.pot_iterations import calculate_iterations_quality, calculate_sp_interval_iters
 from chia.harvester.harvester import Harvester
-from chia.plotting.plot_tools import PlotInfo, parse_plot_info
+from chia.plotting.util import PlotInfo, parse_plot_info
 from chia.protocols import harvester_protocol
 from chia.protocols.farmer_protocol import FarmingInfo
 from chia.protocols.harvester_protocol import Plot
@@ -37,14 +37,11 @@ class HarvesterAPI:
         as well as the farmer pks, which must be put into the plots, before the plotting process begins.
         We cannot use any plots which have different keys in them.
         """
-        self.harvester.farmer_public_keys = harvester_handshake.farmer_public_keys
-        self.harvester.pool_public_keys = harvester_handshake.pool_public_keys
+        self.harvester.plot_manager.set_public_keys(
+            harvester_handshake.farmer_public_keys, harvester_handshake.pool_public_keys
+        )
 
-        await self.harvester.refresh_plots()
-
-        if len(self.harvester.provers) == 0:
-            self.harvester.log.warning("Not farming any plots on this harvester. Check your configuration.")
-            return None
+        self.harvester.plot_manager.start_refreshing()
 
     @peer_required
     @api_request
@@ -63,17 +60,12 @@ class HarvesterAPI:
         4. Looks up the full proof of space in the plot for each quality, approximately 64 reads per quality
         5. Returns the proof of space to the farmer
         """
-        if len(self.harvester.pool_public_keys) == 0 or len(self.harvester.farmer_public_keys) == 0:
+        if not self.harvester.plot_manager.public_keys_available():
             # This means that we have not received the handshake yet
             return None
 
         start = time.time()
         assert len(new_challenge.challenge_hash) == 32
-
-        # Refresh plots to see if there are any new ones
-        if start - self.harvester.last_load_time > self.harvester.plot_load_frequency:
-            await self.harvester.refresh_plots()
-            self.harvester.last_load_time = time.time()
 
         loop = asyncio.get_running_loop()
 
@@ -189,22 +181,23 @@ class HarvesterAPI:
         awaitables = []
         passed = 0
         total = 0
-        for try_plot_filename, try_plot_info in self.harvester.provers.items():
-            try:
-                if try_plot_filename.exists():
-                    # Passes the plot filter (does not check sp filter yet though, since we have not reached sp)
-                    # This is being executed at the beginning of the slot
-                    total += 1
-                    if ProofOfSpace.passes_plot_filter(
-                        self.harvester.constants,
-                        try_plot_info.prover.get_id(),
-                        new_challenge.challenge_hash,
-                        new_challenge.sp_hash,
-                    ):
-                        passed += 1
-                        awaitables.append(lookup_challenge(try_plot_filename, try_plot_info))
-            except Exception as e:
-                self.harvester.log.error(f"Error plot file {try_plot_filename} may no longer exist {e}")
+        with self.harvester.plot_manager:
+            for try_plot_filename, try_plot_info in self.harvester.plot_manager.plots.items():
+                try:
+                    if try_plot_filename.exists():
+                        # Passes the plot filter (does not check sp filter yet though, since we have not reached sp)
+                        # This is being executed at the beginning of the slot
+                        total += 1
+                        if ProofOfSpace.passes_plot_filter(
+                            self.harvester.constants,
+                            try_plot_info.prover.get_id(),
+                            new_challenge.challenge_hash,
+                            new_challenge.sp_hash,
+                        ):
+                            passed += 1
+                            awaitables.append(lookup_challenge(try_plot_filename, try_plot_info))
+                except Exception as e:
+                    self.harvester.log.error(f"Error plot file {try_plot_filename} may no longer exist {e}")
 
         # Concurrently executes all lookups on disk, to take advantage of multiple disk parallelism
         total_proofs_found = 0
@@ -239,7 +232,7 @@ class HarvesterAPI:
         self.harvester.log.info(
             f"{len(awaitables)} plots were eligible for farming {new_challenge.challenge_hash.hex()[:10]}..."
             f" Found {total_proofs_found} proofs. Time: {time.time() - start:.5f} s. "
-            f"Total {len(self.harvester.provers)} plots"
+            f"Total {self.harvester.plot_manager.plot_count()} plots"
         )
 
     @api_request
@@ -250,19 +243,20 @@ class HarvesterAPI:
         be used for pooling.
         """
         plot_filename = Path(request.plot_identifier[64:]).resolve()
-        try:
-            plot_info = self.harvester.provers[plot_filename]
-        except KeyError:
-            self.harvester.log.warning(f"KeyError plot {plot_filename} does not exist.")
-            return None
+        with self.harvester.plot_manager:
+            try:
+                plot_info = self.harvester.plot_manager.plots[plot_filename]
+            except KeyError:
+                self.harvester.log.warning(f"KeyError plot {plot_filename} does not exist.")
+                return None
 
-        # Look up local_sk from plot to save locked memory
-        (
-            pool_public_key_or_puzzle_hash,
-            farmer_public_key,
-            local_master_sk,
-        ) = parse_plot_info(plot_info.prover.get_memo())
-        local_sk = master_sk_to_local_sk(local_master_sk)
+            # Look up local_sk from plot to save locked memory
+            (
+                pool_public_key_or_puzzle_hash,
+                farmer_public_key,
+                local_master_sk,
+            ) = parse_plot_info(plot_info.prover.get_memo())
+            local_sk = master_sk_to_local_sk(local_master_sk)
 
         if isinstance(pool_public_key_or_puzzle_hash, G1Element):
             include_taproot = False

chia/plotting/create_plots.py
@@ -8,7 +8,8 @@ from blspy import AugSchemeMPL, G1Element, PrivateKey
 from chiapos import DiskPlotter
 
 from chia.daemon.keychain_proxy import KeychainProxy, connect_to_keychain_and_validate, wrap_local_keychain
-from chia.plotting.plot_tools import add_plot_directory, stream_plot_info_ph, stream_plot_info_pk
+from chia.plotting.util import add_plot_directory
+from chia.plotting.util import stream_plot_info_ph, stream_plot_info_pk
 from chia.types.blockchain_format.proof_of_space import ProofOfSpace
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.bech32m import decode_puzzle_hash
@@ -237,7 +238,7 @@ async def create_plots(args, keys: PlotKeys, root_path, use_datetime=True, test_
             if resolved_final_dir not in plot_directories_list:
                 # Adds the directory to the plot directories if it is not present
                 log.info(f"Adding directory {resolved_final_dir} to harvester for farming")
-                config = add_plot_directory(resolved_final_dir, root_path)
+                config = add_plot_directory(root_path, resolved_final_dir)
 
         if not full_path.exists():
             log.info(f"Starting plot {i + 1}/{num}")

tests/block_tools.py
@@ -46,7 +46,8 @@ from chia.consensus.pot_iterations import (
 )
 from chia.consensus.vdf_info_computation import get_signage_point_vdf_info
 from chia.full_node.signage_point import SignagePoint
-from chia.plotting.plot_tools import PlotInfo, load_plots, parse_plot_info
+from chia.plotting.util import PlotInfo, PlotsRefreshParameter, PlotRefreshResult, parse_plot_info
+from chia.plotting.manager import PlotManager
 from chia.types.blockchain_format.classgroup import ClassgroupElement
 from chia.types.blockchain_format.coin import Coin, hash_coin_list
 from chia.types.blockchain_format.foliage import Foliage, FoliageBlockData, FoliageTransactionBlock, TransactionsInfo
@@ -246,8 +247,33 @@ class BlockTools:
             shutil.rmtree(plot_dir, ignore_errors=True)
             sys.exit(1)
 
-        _, loaded_plots, _, _ = load_plots({}, {}, self.farmer_pubkeys, self.pool_pubkeys, None, False, self.root_path)
-        self.plots: Dict[Path, PlotInfo] = loaded_plots
+        refresh_done = False
+        refresh_parameter: PlotsRefreshParameter = PlotsRefreshParameter(120, 2, 10)
+
+        def test_callback(update_result: PlotRefreshResult):
+            if update_result.remaining_files == 0:
+                nonlocal refresh_done
+                refresh_done = True
+            else:
+                assert 0 < update_result.loaded_plots <= refresh_parameter.batch_size
+                assert update_result.loaded_plots == update_result.processed_files
+                assert update_result.remaining_files > 0
+                assert update_result.loaded_size > 0
+                assert 0 < update_result.duration < 5
+
+        plot_manager: PlotManager = PlotManager(
+            self.root_path, refresh_parameter=refresh_parameter, refresh_callback=test_callback
+        )
+        plot_manager.set_public_keys(self.farmer_pubkeys, self.pool_pubkeys)
+        plot_manager.start_refreshing()
+
+        retry = 5
+        while not refresh_done and retry > 0:
+            time.sleep(1)
+            retry -= 1
+        assert refresh_done
+        plot_manager.stop_refreshing()
+        self.plots: Dict[Path, PlotInfo] = plot_manager.plots
         # create_plots() updates plot_directories. Ensure we refresh our config to reflect the updated value
         self._config["harvester"]["plot_directories"] = load_config(self.root_path, "config.yaml", "harvester")[
             "plot_directories"

ChainGreenOrg/chaingreen-blockchain

(cherry picked from commit 53149f2e57743e203aba1d25a89786edcf1f78ab)

chaingreen/harvester/harvester_api.py
@@ -7,7 +7,7 @@ from blspy import AugSchemeMPL, G2Element, G1Element
 
 from chaingreen.consensus.pot_iterations import calculate_iterations_quality, calculate_sp_interval_iters
 from chaingreen.harvester.harvester import Harvester
-from chaingreen.plotting.plot_tools import PlotInfo, parse_plot_info
+from chaingreen.plotting.util import PlotInfo, parse_plot_info
 from chaingreen.protocols import harvester_protocol
 from chaingreen.protocols.farmer_protocol import FarmingInfo
 from chaingreen.protocols.harvester_protocol import Plot
@@ -37,14 +37,11 @@ class HarvesterAPI:
         as well as the farmer pks, which must be put into the plots, before the plotting process begins.
         We cannot use any plots which have different keys in them.
         """
-        self.harvester.farmer_public_keys = harvester_handshake.farmer_public_keys
-        self.harvester.pool_public_keys = harvester_handshake.pool_public_keys
+        self.harvester.plot_manager.set_public_keys(
+            harvester_handshake.farmer_public_keys, harvester_handshake.pool_public_keys
+        )
 
-        await self.harvester.refresh_plots()
-
-        if len(self.harvester.provers) == 0:
-            self.harvester.log.warning("Not farming any plots on this harvester. Check your configuration.")
-            return None
+        self.harvester.plot_manager.start_refreshing()
 
     @peer_required
     @api_request
@@ -63,17 +60,12 @@ class HarvesterAPI:
         4. Looks up the full proof of space in the plot for each quality, approximately 64 reads per quality
         5. Returns the proof of space to the farmer
         """
-        if len(self.harvester.pool_public_keys) == 0 or len(self.harvester.farmer_public_keys) == 0:
+        if not self.harvester.plot_manager.public_keys_available():
             # This means that we have not received the handshake yet
             return None
 
         start = time.time()
         assert len(new_challenge.challenge_hash) == 32
-
-        # Refresh plots to see if there are any new ones
-        if start - self.harvester.last_load_time > self.harvester.plot_load_frequency:
-            await self.harvester.refresh_plots()
-            self.harvester.last_load_time = time.time()
 
         loop = asyncio.get_running_loop()
 
@@ -189,22 +181,23 @@ class HarvesterAPI:
         awaitables = []
         passed = 0
         total = 0
-        for try_plot_filename, try_plot_info in self.harvester.provers.items():
-            try:
-                if try_plot_filename.exists():
-                    # Passes the plot filter (does not check sp filter yet though, since we have not reached sp)
-                    # This is being executed at the beginning of the slot
-                    total += 1
-                    if ProofOfSpace.passes_plot_filter(
-                        self.harvester.constants,
-                        try_plot_info.prover.get_id(),
-                        new_challenge.challenge_hash,
-                        new_challenge.sp_hash,
-                    ):
-                        passed += 1
-                        awaitables.append(lookup_challenge(try_plot_filename, try_plot_info))
-            except Exception as e:
-                self.harvester.log.error(f"Error plot file {try_plot_filename} may no longer exist {e}")
+        with self.harvester.plot_manager:
+            for try_plot_filename, try_plot_info in self.harvester.plot_manager.plots.items():
+                try:
+                    if try_plot_filename.exists():
+                        # Passes the plot filter (does not check sp filter yet though, since we have not reached sp)
+                        # This is being executed at the beginning of the slot
+                        total += 1
+                        if ProofOfSpace.passes_plot_filter(
+                            self.harvester.constants,
+                            try_plot_info.prover.get_id(),
+                            new_challenge.challenge_hash,
+                            new_challenge.sp_hash,
+                        ):
+                            passed += 1
+                            awaitables.append(lookup_challenge(try_plot_filename, try_plot_info))
+                except Exception as e:
+                    self.harvester.log.error(f"Error plot file {try_plot_filename} may no longer exist {e}")
 
         # Concurrently executes all lookups on disk, to take advantage of multiple disk parallelism
         total_proofs_found = 0
@@ -239,7 +232,7 @@ class HarvesterAPI:
         self.harvester.log.info(
             f"{len(awaitables)} plots were eligible for farming {new_challenge.challenge_hash.hex()[:10]}..."
             f" Found {total_proofs_found} proofs. Time: {time.time() - start:.5f} s. "
-            f"Total {len(self.harvester.provers)} plots"
+            f"Total {self.harvester.plot_manager.plot_count()} plots"
         )
 
     @api_request
@@ -250,19 +243,20 @@ class HarvesterAPI:
         be used for pooling.
         """
         plot_filename = Path(request.plot_identifier[64:]).resolve()
-        try:
-            plot_info = self.harvester.provers[plot_filename]
-        except KeyError:
-            self.harvester.log.warning(f"KeyError plot {plot_filename} does not exist.")
-            return None
+        with self.harvester.plot_manager:
+            try:
+                plot_info = self.harvester.plot_manager.plots[plot_filename]
+            except KeyError:
+                self.harvester.log.warning(f"KeyError plot {plot_filename} does not exist.")
+                return None
 
-        # Look up local_sk from plot to save locked memory
-        (
-            pool_public_key_or_puzzle_hash,
-            farmer_public_key,
-            local_master_sk,
-        ) = parse_plot_info(plot_info.prover.get_memo())
-        local_sk = master_sk_to_local_sk(local_master_sk)
+            # Look up local_sk from plot to save locked memory
+            (
+                pool_public_key_or_puzzle_hash,
+                farmer_public_key,
+                local_master_sk,
+            ) = parse_plot_info(plot_info.prover.get_memo())
+            local_sk = master_sk_to_local_sk(local_master_sk)
 
         if isinstance(pool_public_key_or_puzzle_hash, G1Element):
             include_taproot = False

chaingreen/plotting/create_plots.py
@@ -8,7 +8,8 @@ from blspy import AugSchemeMPL, G1Element, PrivateKey
 from chiapos import DiskPlotter
 
 from chaingreen.daemon.keychain_proxy import KeychainProxy, connect_to_keychain_and_validate, wrap_local_keychain
-from chaingreen.plotting.plot_tools import add_plot_directory, stream_plot_info_ph, stream_plot_info_pk
+from chaingreen.plotting.util import add_plot_directory
+from chaingreen.plotting.util import stream_plot_info_ph, stream_plot_info_pk
 from chaingreen.types.blockchain_format.proof_of_space import ProofOfSpace
 from chaingreen.types.blockchain_format.sized_bytes import bytes32
 from chaingreen.util.bech32m import decode_puzzle_hash
@@ -237,7 +238,7 @@ async def create_plots(args, keys: PlotKeys, root_path, use_datetime=True, test_
             if resolved_final_dir not in plot_directories_list:
                 # Adds the directory to the plot directories if it is not present
                 log.info(f"Adding directory {resolved_final_dir} to harvester for farming")
-                config = add_plot_directory(resolved_final_dir, root_path)
+                config = add_plot_directory(root_path, resolved_final_dir)
 
         if not full_path.exists():
             log.info(f"Starting plot {i + 1}/{num}")

tests/block_tools.py
@@ -46,7 +46,8 @@ from chaingreen.consensus.pot_iterations import (
 )
 from chaingreen.consensus.vdf_info_computation import get_signage_point_vdf_info
 from chaingreen.full_node.signage_point import SignagePoint
-from chaingreen.plotting.plot_tools import PlotInfo, load_plots, parse_plot_info
+from chaingreen.plotting.util import PlotInfo, PlotsRefreshParameter, PlotRefreshResult, parse_plot_info
+from chaingreen.plotting.manager import PlotManager
 from chaingreen.types.blockchain_format.classgroup import ClassgroupElement
 from chaingreen.types.blockchain_format.coin import Coin, hash_coin_list
 from chaingreen.types.blockchain_format.foliage import Foliage, FoliageBlockData, FoliageTransactionBlock, TransactionsInfo
@@ -241,8 +242,33 @@ class BlockTools:
             shutil.rmtree(plot_dir, ignore_errors=True)
             sys.exit(1)
 
-        _, loaded_plots, _, _ = load_plots({}, {}, self.farmer_pubkeys, self.pool_pubkeys, None, False, self.root_path)
-        self.plots: Dict[Path, PlotInfo] = loaded_plots
+        refresh_done = False
+        refresh_parameter: PlotsRefreshParameter = PlotsRefreshParameter(120, 2, 10)
+
+        def test_callback(update_result: PlotRefreshResult):
+            if update_result.remaining_files == 0:
+                nonlocal refresh_done
+                refresh_done = True
+            else:
+                assert 0 < update_result.loaded_plots <= refresh_parameter.batch_size
+                assert update_result.loaded_plots == update_result.processed_files
+                assert update_result.remaining_files > 0
+                assert update_result.loaded_size > 0
+                assert 0 < update_result.duration < 5
+
+        plot_manager: PlotManager = PlotManager(
+            self.root_path, refresh_parameter=refresh_parameter, refresh_callback=test_callback
+        )
+        plot_manager.set_public_keys(self.farmer_pubkeys, self.pool_pubkeys)
+        plot_manager.start_refreshing()
+
+        retry = 5
+        while not refresh_done and retry > 0:
+            time.sleep(1)
+            retry -= 1
+        assert refresh_done
+        plot_manager.stop_refreshing()
+        self.plots: Dict[Path, PlotInfo] = plot_manager.plots
         # create_plots() updates plot_directories. Ensure we refresh our config to reflect the updated value
         self._config["harvester"]["plot_directories"] = load_config(self.root_path, "config.yaml", "harvester")[
             "plot_directories"