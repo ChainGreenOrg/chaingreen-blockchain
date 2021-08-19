Chia-Network/chia-blockchain

Create SSL keys/certs with proper permissions, also verify/fix existing installs (#7972)
* Create directories with 755 permissions, SSL certs with 644, and keys with 600.

* Check SSL file permissions during chia_init(). Exits if permissions are incorrect.

* Overwrite certs/keys instead of failing to write

* Skip SSL file permission checks on Windows (requires checking ACLs)

* Check SSL file permissions when creating an ssl_context

* Skip check_ssl on Windows.
Handle some SSLInvalidPermissions exceptions.

* Added a few comments

* Added chia init --fix-ssl-permissions option to attempt to fix SSL file permission issues.
Update imported cert permissions when using chia init -c.
Code cleanup/restructuring.

* Return a tuple instead of a list

* LGTM and other minor fixes

* Fixed SSL test breakage when calling ssl_context_for_client. The ca_crt param was being passed in as both the cert and private key, triggering the permission check failure.

* Don't exit if SSL file permissions issues are found

* Tweak the exception types that are raised from traverse_dict

chia/cmds/init_funcs.py
@@ -7,7 +7,13 @@ import yaml
 
 from chia import __version__
 from chia.consensus.coinbase import create_puzzlehash_for_pk
-from chia.ssl.create_ssl import generate_ca_signed_cert, get_chia_ca_crt_key, make_ca_cert
+from chia.ssl.create_ssl import (
+    ensure_ssl_dirs,
+    generate_ca_signed_cert,
+    get_chia_ca_crt_key,
+    make_ca_cert,
+    write_ssl_cert_and_key,
+)
 from chia.util.bech32m import encode_puzzle_hash
 from chia.util.config import (
     create_default_chia_config,
@@ -19,6 +25,15 @@ from chia.util.config import (
 from chia.util.ints import uint32
 from chia.util.keychain import Keychain
 from chia.util.path import mkdir
+from chia.util.ssl import (
+    DEFAULT_PERMISSIONS_CERT_FILE,
+    DEFAULT_PERMISSIONS_KEY_FILE,
+    RESTRICT_MASK_CERT_FILE,
+    RESTRICT_MASK_KEY_FILE,
+    check_and_fix_permissions_for_ssl_file,
+    check_ssl,
+    fix_ssl,
+)
 from chia.wallet.derive_keys import master_sk_to_pool_sk, master_sk_to_wallet_sk
 
 private_node_names = {"full_node", "wallet", "farmer", "harvester", "timelord", "daemon"}
@@ -156,9 +171,9 @@ def migrate_from(
     return 1
 
 
-def create_all_ssl(root: Path):
+def create_all_ssl(root_path: Path):
     # remove old key and crt
-    config_dir = root / "config"
+    config_dir = root_path / "config"
     old_key_path = config_dir / "trusted.key"
     old_crt_path = config_dir / "trusted.crt"
     if old_key_path.exists():
@@ -169,23 +184,19 @@ def create_all_ssl(root: Path):
         os.remove(old_crt_path)
 
     ssl_dir = config_dir / "ssl"
-    if not ssl_dir.exists():
-        ssl_dir.mkdir()
     ca_dir = ssl_dir / "ca"
-    if not ca_dir.exists():
-        ca_dir.mkdir()
+    ensure_ssl_dirs([ssl_dir, ca_dir])
 
     private_ca_key_path = ca_dir / "private_ca.key"
     private_ca_crt_path = ca_dir / "private_ca.crt"
     chia_ca_crt, chia_ca_key = get_chia_ca_crt_key()
     chia_ca_crt_path = ca_dir / "chia_ca.crt"
     chia_ca_key_path = ca_dir / "chia_ca.key"
-    chia_ca_crt_path.write_bytes(chia_ca_crt)
-    chia_ca_key_path.write_bytes(chia_ca_key)
+    write_ssl_cert_and_key(chia_ca_crt_path, chia_ca_crt, chia_ca_key_path, chia_ca_key)
 
     if not private_ca_key_path.exists() or not private_ca_crt_path.exists():
         # Create private CA
-        print(f"Can't find private CA, creating a new one in {root} to generate TLS certificates")
+        print(f"Can't find private CA, creating a new one in {root_path} to generate TLS certificates")
         make_ca_cert(private_ca_crt_path, private_ca_key_path)
         # Create private certs for each node
         ca_key = private_ca_key_path.read_bytes()
@@ -193,7 +204,7 @@ def create_all_ssl(root: Path):
         generate_ssl_for_nodes(ssl_dir, ca_crt, ca_key, True)
     else:
         # This is entered when user copied over private CA
-        print(f"Found private CA in {root}, using it to generate TLS certificates")
+        print(f"Found private CA in {root_path}, using it to generate TLS certificates")
         ca_key = private_ca_key_path.read_bytes()
         ca_crt = private_ca_crt_path.read_bytes()
         generate_ssl_for_nodes(ssl_dir, ca_crt, ca_key, True)
@@ -210,8 +221,7 @@ def generate_ssl_for_nodes(ssl_dir: Path, ca_crt: bytes, ca_key: bytes, private:
 
     for node_name in names:
         node_dir = ssl_dir / node_name
-        if not node_dir.exists():
-            node_dir.mkdir()
+        ensure_ssl_dirs([node_dir])
         if private:
             prefix = "private"
         else:
@@ -224,13 +234,18 @@ def generate_ssl_for_nodes(ssl_dir: Path, ca_crt: bytes, ca_key: bytes, private:
 
 
 def copy_cert_files(cert_path: Path, new_path: Path):
-    for ext in "*.crt", "*.key":
-        for old_path_child in cert_path.glob(ext):
-            new_path_child = new_path / old_path_child.name
-            copy_files_rec(old_path_child, new_path_child)
+    for old_path_child in cert_path.glob("*.crt"):
+        new_path_child = new_path / old_path_child.name
+        copy_files_rec(old_path_child, new_path_child)
+        check_and_fix_permissions_for_ssl_file(new_path_child, RESTRICT_MASK_CERT_FILE, DEFAULT_PERMISSIONS_CERT_FILE)
+
+    for old_path_child in cert_path.glob("*.key"):
+        new_path_child = new_path / old_path_child.name
+        copy_files_rec(old_path_child, new_path_child)
+        check_and_fix_permissions_for_ssl_file(new_path_child, RESTRICT_MASK_KEY_FILE, DEFAULT_PERMISSIONS_KEY_FILE)
 
 
-def init(create_certs: Optional[Path], root_path: Path):
+def init(create_certs: Optional[Path], root_path: Path, fix_ssl_permissions: bool = False):
     if create_certs is not None:
         if root_path.exists():
             if os.path.isdir(create_certs):
@@ -246,13 +261,13 @@ def init(create_certs: Optional[Path], root_path: Path):
         else:
             print(f"** {root_path} does not exist. Executing core init **")
             # sanity check here to prevent infinite recursion
-            if chia_init(root_path) == 0 and root_path.exists():
-                return init(create_certs, root_path)
+            if chia_init(root_path, fix_ssl_permissions=fix_ssl_permissions) == 0 and root_path.exists():
+                return init(create_certs, root_path, fix_ssl_permissions)
 
             print(f"** {root_path} was not created. Exiting **")
             return -1
     else:
-        return chia_init(root_path)
+        return chia_init(root_path, fix_ssl_permissions=fix_ssl_permissions)
 
 
 def chia_version_number() -> Tuple[str, str, str, str]:
@@ -314,7 +329,9 @@ def chia_full_version_str() -> str:
     return f"{major}.{minor}.{patch}{dev}"
 
 
-def chia_init(root_path: Path, *, should_check_keys: bool = True):
+def chia_init(
+    root_path: Path, *, should_check_keys: bool = True, should_check_ssl: bool = True, fix_ssl_permissions: bool = False
+):
     """
     Standard first run initialization or migration steps. Handles config creation,
     generation of SSL certs, and setting target addresses (via check_keys).
@@ -334,6 +351,10 @@ def chia_init(root_path: Path, *, should_check_keys: bool = True):
     if root_path.is_dir() and Path(root_path / "config" / "config.yaml").exists():
         # This is reached if CHIA_ROOT is set, or if user has run chia init twice
         # before a new update.
+        if fix_ssl_permissions:
+            fix_ssl(root_path)
+        elif should_check_ssl:
+            check_ssl(root_path)
         if should_check_keys:
             check_keys(root_path)
         print(f"{root_path} already exists, no migration action taken")
@@ -341,6 +362,10 @@ def chia_init(root_path: Path, *, should_check_keys: bool = True):
 
     create_default_chia_config(root_path)
     create_all_ssl(root_path)
+    if fix_ssl_permissions:
+        fix_ssl(root_path)
+    elif should_check_ssl:
+        check_ssl(root_path)
     if should_check_keys:
         check_keys(root_path)
     print("")

tests/core/ssl/test_ssl.py
@@ -96,7 +96,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            farmer_server.ca_private_crt_path, farmer_server.ca_private_crt_path, priv_crt, priv_key
+            farmer_server.ca_private_crt_path, farmer_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(farmer_server, 12312, ssl_context)
         assert connected is True
@@ -108,12 +108,12 @@ class TestSSL:
             farmer_server.chia_ca_crt_path.read_bytes(), farmer_server.chia_ca_key_path.read_bytes(), pub_crt, pub_key
         )
         ssl_context = ssl_context_for_client(
-            farmer_server.chia_ca_crt_path, farmer_server.chia_ca_crt_path, pub_crt, pub_key
+            farmer_server.chia_ca_crt_path, farmer_server.chia_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(farmer_server, 12312, ssl_context)
         assert connected is False
         ssl_context = ssl_context_for_client(
-            farmer_server.ca_private_crt_path, farmer_server.ca_private_crt_path, pub_crt, pub_key
+            farmer_server.ca_private_crt_path, farmer_server.ca_private_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(farmer_server, 12312, ssl_context)
         assert connected is False
@@ -134,7 +134,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            full_node_server.chia_ca_crt_path, full_node_server.chia_ca_crt_path, pub_crt, pub_key
+            full_node_server.chia_ca_crt_path, full_node_server.chia_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(full_node_server, 12312, ssl_context)
         assert connected is True
@@ -151,7 +151,7 @@ class TestSSL:
             wallet_server.chia_ca_crt_path.read_bytes(), wallet_server.chia_ca_key_path.read_bytes(), pub_crt, pub_key
         )
         ssl_context = ssl_context_for_client(
-            wallet_server.chia_ca_crt_path, wallet_server.chia_ca_crt_path, pub_crt, pub_key
+            wallet_server.chia_ca_crt_path, wallet_server.chia_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(wallet_server, 12312, ssl_context)
         assert connected is False
@@ -166,7 +166,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            wallet_server.ca_private_crt_path, wallet_server.ca_private_crt_path, priv_crt, priv_key
+            wallet_server.ca_private_crt_path, wallet_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(wallet_server, 12312, ssl_context)
         assert connected is False
@@ -186,7 +186,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            harvester_server.chia_ca_crt_path, harvester_server.chia_ca_crt_path, pub_crt, pub_key
+            harvester_server.chia_ca_crt_path, harvester_server.chia_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(harvester_server, 12312, ssl_context)
         assert connected is False
@@ -201,7 +201,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            harvester_server.ca_private_crt_path, harvester_server.ca_private_crt_path, priv_crt, priv_key
+            harvester_server.ca_private_crt_path, harvester_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(harvester_server, 12312, ssl_context)
         assert connected is False
@@ -220,7 +220,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            introducer_server.chia_ca_crt_path, introducer_server.chia_ca_crt_path, pub_crt, pub_key
+            introducer_server.chia_ca_crt_path, introducer_server.chia_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(introducer_server, 12312, ssl_context)
         assert connected is True
@@ -239,7 +239,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            timelord_server.chia_ca_crt_path, timelord_server.chia_ca_crt_path, pub_crt, pub_key
+            timelord_server.chia_ca_crt_path, timelord_server.chia_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(timelord_server, 12312, ssl_context)
         assert connected is False
@@ -254,7 +254,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            timelord_server.ca_private_crt_path, timelord_server.ca_private_crt_path, priv_crt, priv_key
+            timelord_server.ca_private_crt_path, timelord_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(timelord_server, 12312, ssl_context)
         assert connected is False

ChainGreenOrg/chaingreen-blockchain

(cherry picked from commit 39fbe2aa17c4e473bca49ea3f7d562ffddb73f38)

chaingreen/cmds/init_funcs.py
@@ -7,7 +7,13 @@ import yaml
 
 from chaingreen import __version__
 from chaingreen.consensus.coinbase import create_puzzlehash_for_pk
-from chaingreen.ssl.create_ssl import generate_ca_signed_cert, get_chaingreen_ca_crt_key, make_ca_cert
+from chaingreen.ssl.create_ssl import (
+    ensure_ssl_dirs,
+    generate_ca_signed_cert,
+    get_chaingreen_ca_crt_key,
+    make_ca_cert,
+    write_ssl_cert_and_key,
+)
 from chaingreen.util.bech32m import encode_puzzle_hash
 from chaingreen.util.config import (
     create_default_chaingreen_config,
@@ -19,6 +25,15 @@ from chaingreen.util.config import (
 from chaingreen.util.ints import uint32
 from chaingreen.util.keychain import Keychain
 from chaingreen.util.path import mkdir
+from chaingreen.util.ssl import (
+    DEFAULT_PERMISSIONS_CERT_FILE,
+    DEFAULT_PERMISSIONS_KEY_FILE,
+    RESTRICT_MASK_CERT_FILE,
+    RESTRICT_MASK_KEY_FILE,
+    check_and_fix_permissions_for_ssl_file,
+    check_ssl,
+    fix_ssl,
+)
 from chaingreen.wallet.derive_keys import master_sk_to_pool_sk, master_sk_to_wallet_sk
 
 private_node_names = {"full_node", "wallet", "farmer", "harvester", "timelord", "daemon"}
@@ -156,9 +171,9 @@ def migrate_from(
     return 1
 
 
-def create_all_ssl(root: Path):
+def create_all_ssl(root_path: Path):
     # remove old key and crt
-    config_dir = root / "config"
+    config_dir = root_path / "config"
     old_key_path = config_dir / "trusted.key"
     old_crt_path = config_dir / "trusted.crt"
     if old_key_path.exists():
@@ -169,23 +184,19 @@ def create_all_ssl(root: Path):
         os.remove(old_crt_path)
 
     ssl_dir = config_dir / "ssl"
-    if not ssl_dir.exists():
-        ssl_dir.mkdir()
     ca_dir = ssl_dir / "ca"
-    if not ca_dir.exists():
-        ca_dir.mkdir()
+    ensure_ssl_dirs([ssl_dir, ca_dir])
 
     private_ca_key_path = ca_dir / "private_ca.key"
     private_ca_crt_path = ca_dir / "private_ca.crt"
     chaingreen_ca_crt, chaingreen_ca_key = get_chaingreen_ca_crt_key()
     chaingreen_ca_crt_path = ca_dir / "chaingreen_ca.crt"
     chaingreen_ca_key_path = ca_dir / "chaingreen_ca.key"
-    chaingreen_ca_crt_path.write_bytes(chaingreen_ca_crt)
-    chaingreen_ca_key_path.write_bytes(chaingreen_ca_key)
+    write_ssl_cert_and_key(chaingreen_ca_crt_path, chaingreen_ca_crt, chaingreen_ca_key_path, chaingreen_ca_key)
 
     if not private_ca_key_path.exists() or not private_ca_crt_path.exists():
         # Create private CA
-        print(f"Can't find private CA, creating a new one in {root} to generate TLS certificates")
+        print(f"Can't find private CA, creating a new one in {root_path} to generate TLS certificates")
         make_ca_cert(private_ca_crt_path, private_ca_key_path)
         # Create private certs for each node
         ca_key = private_ca_key_path.read_bytes()
@@ -193,7 +204,7 @@ def create_all_ssl(root: Path):
         generate_ssl_for_nodes(ssl_dir, ca_crt, ca_key, True)
     else:
         # This is entered when user copied over private CA
-        print(f"Found private CA in {root}, using it to generate TLS certificates")
+        print(f"Found private CA in {root_path}, using it to generate TLS certificates")
         ca_key = private_ca_key_path.read_bytes()
         ca_crt = private_ca_crt_path.read_bytes()
         generate_ssl_for_nodes(ssl_dir, ca_crt, ca_key, True)
@@ -210,8 +221,7 @@ def generate_ssl_for_nodes(ssl_dir: Path, ca_crt: bytes, ca_key: bytes, private:
 
     for node_name in names:
         node_dir = ssl_dir / node_name
-        if not node_dir.exists():
-            node_dir.mkdir()
+        ensure_ssl_dirs([node_dir])
         if private:
             prefix = "private"
         else:
@@ -224,13 +234,18 @@ def generate_ssl_for_nodes(ssl_dir: Path, ca_crt: bytes, ca_key: bytes, private:
 
 
 def copy_cert_files(cert_path: Path, new_path: Path):
-    for ext in "*.crt", "*.key":
-        for old_path_child in cert_path.glob(ext):
-            new_path_child = new_path / old_path_child.name
-            copy_files_rec(old_path_child, new_path_child)
+    for old_path_child in cert_path.glob("*.crt"):
+        new_path_child = new_path / old_path_child.name
+        copy_files_rec(old_path_child, new_path_child)
+        check_and_fix_permissions_for_ssl_file(new_path_child, RESTRICT_MASK_CERT_FILE, DEFAULT_PERMISSIONS_CERT_FILE)
+
+    for old_path_child in cert_path.glob("*.key"):
+        new_path_child = new_path / old_path_child.name
+        copy_files_rec(old_path_child, new_path_child)
+        check_and_fix_permissions_for_ssl_file(new_path_child, RESTRICT_MASK_KEY_FILE, DEFAULT_PERMISSIONS_KEY_FILE)
 
 
-def init(create_certs: Optional[Path], root_path: Path):
+def init(create_certs: Optional[Path], root_path: Path, fix_ssl_permissions: bool = False):
     if create_certs is not None:
         if root_path.exists():
             if os.path.isdir(create_certs):
@@ -246,13 +261,13 @@ def init(create_certs: Optional[Path], root_path: Path):
         else:
             print(f"** {root_path} does not exist. Executing core init **")
             # sanity check here to prevent infinite recursion
-            if chia_init(root_path) == 0 and root_path.exists():
-                return init(create_certs, root_path)
+            if chia_init(root_path, fix_ssl_permissions=fix_ssl_permissions) == 0 and root_path.exists():
+                return init(create_certs, root_path, fix_ssl_permissions)
 
             print(f"** {root_path} was not created. Exiting **")
             return -1
     else:
-        return chaingreen_init(root_path)
+        return chia_init(root_path, fix_ssl_permissions=fix_ssl_permissions)
 
 
 def chaingreen_version_number() -> Tuple[str, str, str, str]:
@@ -314,7 +329,9 @@ def chaingreen_full_version_str() -> str:
     return f"{major}.{minor}.{patch}{dev}"
 
 
-def chia_init(root_path: Path, *, should_check_keys: bool = True):
+def chia_init(
+    root_path: Path, *, should_check_keys: bool = True, should_check_ssl: bool = True, fix_ssl_permissions: bool = False
+):
     """
     Standard first run initialization or migration steps. Handles config creation,
     generation of SSL certs, and setting target addresses (via check_keys).
@@ -334,6 +351,10 @@ def chia_init(root_path: Path, *, should_check_keys: bool = True):
     if root_path.is_dir() and Path(root_path / "config" / "config.yaml").exists():
         # This is reached if CHAINGREEN_ROOT is set, or if user has run chaingreen init twice
         # before a new update.
+        if fix_ssl_permissions:
+            fix_ssl(root_path)
+        elif should_check_ssl:
+            check_ssl(root_path)
         if should_check_keys:
             check_keys(root_path)
         print(f"{root_path} already exists, no migration action taken")
@@ -341,6 +362,10 @@ def chia_init(root_path: Path, *, should_check_keys: bool = True):
 
     create_default_chaingreen_config(root_path)
     create_all_ssl(root_path)
+    if fix_ssl_permissions:
+        fix_ssl(root_path)
+    elif should_check_ssl:
+        check_ssl(root_path)
     if should_check_keys:
         check_keys(root_path)
     print("")

tests/core/ssl/test_ssl.py
@@ -96,7 +96,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            farmer_server.ca_private_crt_path, farmer_server.ca_private_crt_path, priv_crt, priv_key
+            farmer_server.ca_private_crt_path, farmer_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(farmer_server, 12312, ssl_context)
         assert connected is True
@@ -111,12 +111,12 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            farmer_server.chaingreen_ca_crt_path, farmer_server.chaingreen_ca_crt_path, pub_crt, pub_key
+            farmer_server.chaingreen_ca_crt_path, farmer_server.chaingreen_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(farmer_server, 12312, ssl_context)
         assert connected is False
         ssl_context = ssl_context_for_client(
-            farmer_server.ca_private_crt_path, farmer_server.ca_private_crt_path, pub_crt, pub_key
+            farmer_server.ca_private_crt_path, farmer_server.ca_private_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(farmer_server, 12312, ssl_context)
         assert connected is False
@@ -137,7 +137,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            full_node_server.chaingreen_ca_crt_path, full_node_server.chaingreen_ca_crt_path, pub_crt, pub_key
+            full_node_server.chaingreen_ca_crt_path, full_node_server.chaingreen_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(full_node_server, 12312, ssl_context)
         assert connected is True
@@ -157,7 +157,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            wallet_server.chaingreen_ca_crt_path, wallet_server.chaingreen_ca_crt_path, pub_crt, pub_key
+            wallet_server.chaingreen_ca_crt_path, wallet_server.chaingreen_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(wallet_server, 12312, ssl_context)
         assert connected is False
@@ -172,7 +172,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            wallet_server.ca_private_crt_path, wallet_server.ca_private_crt_path, priv_crt, priv_key
+            wallet_server.ca_private_crt_path, wallet_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(wallet_server, 12312, ssl_context)
         assert connected is False
@@ -192,7 +192,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            harvester_server.chaingreen_ca_crt_path, harvester_server.chaingreen_ca_crt_path, pub_crt, pub_key
+            harvester_server.chaingreen_ca_crt_path, harvester_server.chaingreen_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(harvester_server, 12312, ssl_context)
         assert connected is False
@@ -207,7 +207,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            harvester_server.ca_private_crt_path, harvester_server.ca_private_crt_path, priv_crt, priv_key
+            harvester_server.ca_private_crt_path, harvester_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(harvester_server, 12312, ssl_context)
         assert connected is False
@@ -226,7 +226,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            introducer_server.chaingreen_ca_crt_path, introducer_server.chaingreen_ca_crt_path, pub_crt, pub_key
+            introducer_server.chaingreen_ca_crt_path, introducer_server.chaingreen_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(introducer_server, 12312, ssl_context)
         assert connected is True
@@ -245,7 +245,7 @@ class TestSSL:
             pub_key,
         )
         ssl_context = ssl_context_for_client(
-            timelord_server.chaingreen_ca_crt_path, timelord_server.chaingreen_ca_crt_path, pub_crt, pub_key
+            timelord_server.chaingreen_ca_crt_path, timelord_server.chaingreen_ca_key_path, pub_crt, pub_key
         )
         connected = await establish_connection(timelord_server, 12312, ssl_context)
         assert connected is False
@@ -260,7 +260,7 @@ class TestSSL:
             priv_key,
         )
         ssl_context = ssl_context_for_client(
-            timelord_server.ca_private_crt_path, timelord_server.ca_private_crt_path, priv_crt, priv_key
+            timelord_server.ca_private_crt_path, timelord_server.ca_private_key_path, priv_crt, priv_key
         )
         connected = await establish_connection(timelord_server, 12312, ssl_context)
         assert connected is False