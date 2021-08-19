Chia-Network/chia-blockchain

don't save unknown conditions (#7548)
* don't save unknown conditions

* remove UNKNOWN enum from ConditionOpcode enum

tests/core/full_node/test_mempool.py
@@ -26,7 +26,7 @@ from chia.util.ints import uint64
 from chia.util.hash import std_hash
 from chia.types.mempool_inclusion_status import MempoolInclusionStatus
 from chia.util.api_decorators import api_request, peer_required, bytes_required
-from chia.full_node.mempool_check_conditions import parse_condition_args
+from chia.full_node.mempool_check_conditions import parse_condition_args, parse_condition
 
 from tests.connection_utils import connect_and_get_peer
 from tests.core.node_height import node_height_at_least
@@ -1548,20 +1548,6 @@ class TestMempoolManager:
         assert status == MempoolInclusionStatus.FAILED
         assert err == Err.ASSERT_MY_AMOUNT_FAILED
 
-    @pytest.mark.asyncio
-    async def test_unknown_condition(self, two_nodes):
-
-        full_node_1, full_node_2, server_1, server_2 = two_nodes
-        cvp = ConditionWithArgs(ConditionOpcode.UNKNOWN, [])
-        dic = {cvp.opcode: [cvp]}
-        blocks, spend_bundle1, peer, status, err = await self.condition_tester(two_nodes, dic)
-
-        sb1 = full_node_1.full_node.mempool_manager.get_spendbundle(spend_bundle1.name())
-
-        assert sb1 is None
-        assert status == MempoolInclusionStatus.FAILED
-        assert err == Err.INVALID_CONDITION
-
 
 class TestConditionParser:
     @pytest.mark.parametrize("safe_mode", [True, False])
@@ -1932,6 +1918,22 @@ class TestConditionParser:
 
             with pytest.raises(ValidationError):
                 cost, args = parse_condition_args(SExp.to([]), opcode, False)
+
+    def test_parse_condition(self):
+
+        for opcode in [129, 0, 1, 1000, 74]:
+            with pytest.raises(ValidationError):
+                cost, args = parse_condition(SExp.to([int_to_bytes(opcode), b"test"]), safe_mode=True)
+
+            with pytest.raises(ValidationError):
+                cost, args = parse_condition(SExp.to([int_to_bytes(opcode), b"foo", b"bar"]), safe_mode=True)
+
+            with pytest.raises(ValidationError):
+                cost, args = parse_condition(SExp.to([int_to_bytes(opcode)]), safe_mode=True)
+
+            assert (0, None) == parse_condition(SExp.to([int_to_bytes(opcode), b"test"]), safe_mode=False)
+            assert (0, None) == parse_condition(SExp.to([int_to_bytes(opcode), b"foo", b"bar"]), safe_mode=False)
+            assert (0, None) == parse_condition(SExp.to([int_to_bytes(opcode)]), safe_mode=False)
 
 
 # the following tests generate generator programs and run them through get_name_puzzle_conditions()

ChainGreenOrg/chaingreen-blockchain

(cherry picked from commit 485fab80b7ad4f71ae88cfc6769f4d7d11e18617)

tests/core/full_node/test_mempool.py
@@ -26,7 +26,7 @@ from chaingreen.util.ints import uint64
 from chaingreen.util.hash import std_hash
 from chaingreen.types.mempool_inclusion_status import MempoolInclusionStatus
 from chaingreen.util.api_decorators import api_request, peer_required, bytes_required
-from chaingreen.full_node.mempool_check_conditions import parse_condition_args
+from chaingreen.full_node.mempool_check_conditions import parse_condition_args, parse_condition
 
 from tests.connection_utils import connect_and_get_peer
 from tests.core.node_height import node_height_at_least
@@ -1548,20 +1548,6 @@ class TestMempoolManager:
         assert status == MempoolInclusionStatus.FAILED
         assert err == Err.ASSERT_MY_AMOUNT_FAILED
 
-    @pytest.mark.asyncio
-    async def test_unknown_condition(self, two_nodes):
-
-        full_node_1, full_node_2, server_1, server_2 = two_nodes
-        cvp = ConditionWithArgs(ConditionOpcode.UNKNOWN, [])
-        dic = {cvp.opcode: [cvp]}
-        blocks, spend_bundle1, peer, status, err = await self.condition_tester(two_nodes, dic)
-
-        sb1 = full_node_1.full_node.mempool_manager.get_spendbundle(spend_bundle1.name())
-
-        assert sb1 is None
-        assert status == MempoolInclusionStatus.FAILED
-        assert err == Err.INVALID_CONDITION
-
 
 class TestConditionParser:
     @pytest.mark.parametrize("safe_mode", [True, False])
@@ -1932,6 +1918,22 @@ class TestConditionParser:
 
             with pytest.raises(ValidationError):
                 cost, args = parse_condition_args(SExp.to([]), opcode, False)
+
+    def test_parse_condition(self):
+
+        for opcode in [129, 0, 1, 1000, 74]:
+            with pytest.raises(ValidationError):
+                cost, args = parse_condition(SExp.to([int_to_bytes(opcode), b"test"]), safe_mode=True)
+
+            with pytest.raises(ValidationError):
+                cost, args = parse_condition(SExp.to([int_to_bytes(opcode), b"foo", b"bar"]), safe_mode=True)
+
+            with pytest.raises(ValidationError):
+                cost, args = parse_condition(SExp.to([int_to_bytes(opcode)]), safe_mode=True)
+
+            assert (0, None) == parse_condition(SExp.to([int_to_bytes(opcode), b"test"]), safe_mode=False)
+            assert (0, None) == parse_condition(SExp.to([int_to_bytes(opcode), b"foo", b"bar"]), safe_mode=False)
+            assert (0, None) == parse_condition(SExp.to([int_to_bytes(opcode)]), safe_mode=False)
 
 
 # the following tests generate generator programs and run them through get_name_puzzle_conditions()