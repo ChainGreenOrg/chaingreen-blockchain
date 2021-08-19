Chia-Network/chia-blockchain

Partial reversion of CoinSpend rename for API compatibility (#7636)
* Partial reversion of the coin spend rename

* Make `SpendBundle` json-serialize the classic way by default.

Co-authored-by: Richard Kiss <him@richardkiss.com>

chia/types/spend_bundle.py
@@ -1,3 +1,6 @@
+import dataclasses
+import warnings
+
 from dataclasses import dataclass
 from typing import List
 
@@ -5,7 +8,7 @@ from blspy import AugSchemeMPL, G2Element
 
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.util.streamable import Streamable, streamable
+from chia.util.streamable import Streamable, dataclass_from_dict, recurse_jsonify, streamable
 from chia.wallet.util.debug_spend_bundle import debug_spend_bundle
 
 from .coin_spend import CoinSpend
@@ -23,6 +26,10 @@ class SpendBundle(Streamable):
 
     coin_spends: List[CoinSpend]
     aggregated_signature: G2Element
+
+    @property
+    def coin_solutions(self):
+        return self.coin_spends
 
     @classmethod
     def aggregate(cls, spend_bundles) -> "SpendBundle":
@@ -68,3 +75,34 @@ class SpendBundle(Streamable):
             result.append(add)
 
         return result
+
+    # Note that `coin_spends` used to have the bad name `coin_solutions`.
+    # Some API still expects this name. For now, we accept both names.
+    #
+    # TODO: continue this deprecation. Eventually, all code below here should be removed.
+    #  1. set `exclude_modern_keys` to `False` (and manually set to `True` where necessary)
+    #  2. set `include_legacy_keys` to `False` (and manually set to `False` where necessary)
+    #  3. remove all references to `include_legacy_keys=True`
+    #  4. remove all code below this point
+
+    @classmethod
+    def from_json_dict(cls, json_dict):
+        if "coin_solutions" in json_dict:
+            if "coin_spends" not in json_dict:
+                json_dict = dict(
+                    aggregated_signature=json_dict["aggregated_signature"], coin_spends=json_dict["coin_solutions"]
+                )
+                warnings.warn("`coin_solutions` is now `coin_spends` in `SpendBundle.from_json_dict`")
+            else:
+                raise ValueError("JSON contains both `coin_solutions` and `coin_spends`, just use `coin_spends`")
+        return dataclass_from_dict(cls, json_dict)
+
+    def to_json_dict(self, include_legacy_keys: bool = True, exclude_modern_keys: bool = True):
+        if include_legacy_keys is False and exclude_modern_keys is True:
+            raise ValueError("`coin_spends` not included in legacy or modern outputs")
+        d = dataclasses.asdict(self)
+        if include_legacy_keys:
+            d["coin_solutions"] = d["coin_spends"]
+        if exclude_modern_keys:
+            del d["coin_spends"]
+        return recurse_jsonify(d)

ChainGreenOrg/chaingreen-blockchain

(cherry picked from commit 30cb11a051cd10874410d902a01109ff56c3b9f3)

chaingreen/types/spend_bundle.py
@@ -1,3 +1,6 @@
+import dataclasses
+import warnings
+
 from dataclasses import dataclass
 from typing import List
 
@@ -5,7 +8,7 @@ from blspy import AugSchemeMPL, G2Element
 
 from chaingreen.types.blockchain_format.coin import Coin
 from chaingreen.types.blockchain_format.sized_bytes import bytes32
-from chaingreen.util.streamable import Streamable, streamable
+from chaingreen.util.streamable import Streamable, dataclass_from_dict, recurse_jsonify, streamable
 from chaingreen.wallet.cc_wallet.debug_spend_bundle import debug_spend_bundle
 
 from .coin_spend import CoinSpend
@@ -23,6 +26,10 @@ class SpendBundle(Streamable):
 
     coin_spends: List[CoinSpend]
     aggregated_signature: G2Element
+
+    @property
+    def coin_solutions(self):
+        return self.coin_spends
 
     @classmethod
     def aggregate(cls, spend_bundles) -> "SpendBundle":
@@ -68,3 +75,34 @@ class SpendBundle(Streamable):
             result.append(add)
 
         return result
+
+    # Note that `coin_spends` used to have the bad name `coin_solutions`.
+    # Some API still expects this name. For now, we accept both names.
+    #
+    # TODO: continue this deprecation. Eventually, all code below here should be removed.
+    #  1. set `exclude_modern_keys` to `False` (and manually set to `True` where necessary)
+    #  2. set `include_legacy_keys` to `False` (and manually set to `False` where necessary)
+    #  3. remove all references to `include_legacy_keys=True`
+    #  4. remove all code below this point
+
+    @classmethod
+    def from_json_dict(cls, json_dict):
+        if "coin_solutions" in json_dict:
+            if "coin_spends" not in json_dict:
+                json_dict = dict(
+                    aggregated_signature=json_dict["aggregated_signature"], coin_spends=json_dict["coin_solutions"]
+                )
+                warnings.warn("`coin_solutions` is now `coin_spends` in `SpendBundle.from_json_dict`")
+            else:
+                raise ValueError("JSON contains both `coin_solutions` and `coin_spends`, just use `coin_spends`")
+        return dataclass_from_dict(cls, json_dict)
+
+    def to_json_dict(self, include_legacy_keys: bool = True, exclude_modern_keys: bool = True):
+        if include_legacy_keys is False and exclude_modern_keys is True:
+            raise ValueError("`coin_spends` not included in legacy or modern outputs")
+        d = dataclasses.asdict(self)
+        if include_legacy_keys:
+            d["coin_solutions"] = d["coin_spends"]
+        if exclude_modern_keys:
+            del d["coin_spends"]
+        return recurse_jsonify(d)