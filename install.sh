e20760e533fab3e002db3eda4b245b57edc5279d
Chris Marslender <chris@chia.net>
2021-09-17 13:59:09 -0700

    Update channel to direct users to in install.sh (#8517)

--- install.sh
+++ install.sh
@@ -120,8 +120,8 @@ python -m pip install --extra-index-url https://pypi.chia.net/simple/ miniupnpc=
 python -m pip install -e . --extra-index-url https://pypi.chia.net/simple/
 
 echo ""
-echo "Chaingreen blockchain install.sh complete."
-echo "For assistance join us on Keybase in the #testnet chat channel:"
+echo "Chia blockchain install.sh complete."
+echo "For assistance join us on Keybase in the #support chat channel:"
 echo "https://keybase.io/team/chia_network.public"
 echo ""
 echo "Try the Quick Start Guide to running chia-blockchain:"
