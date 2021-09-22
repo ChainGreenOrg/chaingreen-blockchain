e65429fe7229202349e9a36de7138d62b5e9cd14
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2021-09-22 13:44:29 -0400

    Improve and fix outdated messages in CLI. (#8553)
    * Improve and fix outdated messages in CLI.
    
    * Update chia/cmds/init_funcs.py
    
    Good catch
    
    Co-authored-by: Jeff Cruikshank <paninaro@gmail.com>
    
    Co-authored-by: Jack Nelson <jackmiami60612@gmail.com>
    Co-authored-by: Jeff Cruikshank <paninaro@gmail.com>

--- chaingreen/cmds/init_funcs.py
+++ chaingreen/cmds/init_funcs.py
@@ -88,11 +88,15 @@ def check_keys(new_root: Path, keychain: Optional[Keychain] = None) -> None:
             if all_targets[-1] == config["pool"].get("cgn_target_address"):
                 stop_searching_for_pool = True
 
-    # Set the destinations
-    if "cgn_target_address" not in config["farmer"]:
-        print(f"Setting the cgn destination address for coinbase fees reward to {all_targets[0]}")
-        config["farmer"]["cgn_target_address"] = all_targets[0]
-    elif config["farmer"]["cgn_target_address"] not in all_targets:
+    # Set the destinations, if necessary
+    updated_target: bool = False
+    if "xch_target_address" not in config["farmer"]:
+        print(
+            f"Setting the xch destination for the farmer reward (1/8 plus fees, solo and pooling) to {all_targets[0]}"
+        )
+        config["farmer"]["xch_target_address"] = all_targets[0]
+        updated_target = True
+    elif config["farmer"]["xch_target_address"] not in all_targets:
         print(
             f"WARNING: using a farmer address which we don't have the private"
             f" keys for. We searched the first {number_of_ph_to_search} addresses. Consider overriding "
@@ -101,14 +105,20 @@ def check_keys(new_root: Path, keychain: Optional[Keychain] = None) -> None:
 
     if "pool" not in config:
         config["pool"] = {}
-    if "cgn_target_address" not in config["pool"]:
-        print(f"Setting the cgn destination address for coinbase reward to {all_targets[0]}")
-        config["pool"]["cgn_target_address"] = all_targets[0]
-    elif config["pool"]["cgn_target_address"] not in all_targets:
+    if "xch_target_address" not in config["pool"]:
+        print(f"Setting the xch destination address for pool reward (7/8 for solo only) to {all_targets[0]}")
+        config["pool"]["xch_target_address"] = all_targets[0]
+        updated_target = True
+    elif config["pool"]["xch_target_address"] not in all_targets:
         print(
             f"WARNING: using a pool address which we don't have the private"
             f" keys for. We searched the first {number_of_ph_to_search} addresses. Consider overriding "
             f"{config['pool']['cgn_target_address']} with {all_targets[0]}"
+        )
+    if updated_target:
+        print(
+            f"To change the XCH destination addresses, edit the `xch_target_address` entries in"
+            f" {(new_root / 'config' / 'config.yaml').absolute()}."
         )
 
     # Set the pool pks in the farmer
