Chia-Network/chia-blockchain

chia|tests: Add `node_type` parameter to `get_connections` RPC (#7492)
* rpc|server: Add Optional `node_type` paramter to `get_connections`

* farmer: Query `HARVESTER` connections only

* tests: Basic test for `node_type` parameter

chia/rpc/rpc_client.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional, Any
 
 import aiohttp
 
-from chia.server.server import ssl_context_for_client
+from chia.server.server import NodeType, ssl_context_for_client
 from chia.server.ssl_context import private_ssl_ca_paths
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.byte_types import hexstr_to_bytes
@@ -45,8 +45,11 @@ class RpcClient:
                 raise ValueError(res_json)
             return res_json
 
-    async def get_connections(self) -> List[Dict]:
-        response = await self.fetch("get_connections", {})
+    async def get_connections(self, node_type: Optional[NodeType] = None) -> List[Dict]:
+        request = {}
+        if node_type is not None:
+            request["node_type"] = node_type.value
+        response = await self.fetch("get_connections", request)
         for connection in response["connections"]:
             connection["node_id"] = hexstr_to_bytes(connection["node_id"])
         return response["connections"]

tests/core/test_full_node_rpc.py
@@ -9,7 +9,7 @@ from chia.full_node.signage_point import SignagePoint
 from chia.protocols import full_node_protocol
 from chia.rpc.full_node_rpc_api import FullNodeRpcApi
 from chia.rpc.full_node_rpc_client import FullNodeRpcClient
-from chia.rpc.rpc_server import start_rpc_server
+from chia.rpc.rpc_server import NodeType, start_rpc_server
 from chia.simulator.simulator_protocol import FarmNewBlockProtocol
 from chia.types.spend_bundle import SpendBundle
 from chia.types.unfinished_block import UnfinishedBlock
@@ -192,7 +192,9 @@ class TestRpc:
 
             await time_out_assert(10, num_connections, 1)
             connections = await client.get_connections()
-
+            assert NodeType(connections[0]["type"]) == NodeType.FULL_NODE.value
+            assert len(await client.get_connections(NodeType.FULL_NODE)) == 1
+            assert len(await client.get_connections(NodeType.FARMER)) == 0
             await client.close_connection(connections[0]["node_id"])
             await time_out_assert(10, num_connections, 0)
         finally:

ChainGreenOrg/chaingreen-blockchain

(cherry picked from commit 1b196e6bd8dcd1952a10ebda1d6a74a0706457c2)

chaingreen/rpc/rpc_client.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional, Any
 
 import aiohttp
 
-from chaingreen.server.server import ssl_context_for_client
+from chaingreen.server.server import NodeType, ssl_context_for_client
 from chaingreen.server.ssl_context import private_ssl_ca_paths
 from chaingreen.types.blockchain_format.sized_bytes import bytes32
 from chaingreen.util.byte_types import hexstr_to_bytes
@@ -45,8 +45,11 @@ class RpcClient:
                 raise ValueError(res_json)
             return res_json
 
-    async def get_connections(self) -> List[Dict]:
-        response = await self.fetch("get_connections", {})
+    async def get_connections(self, node_type: Optional[NodeType] = None) -> List[Dict]:
+        request = {}
+        if node_type is not None:
+            request["node_type"] = node_type.value
+        response = await self.fetch("get_connections", request)
         for connection in response["connections"]:
             connection["node_id"] = hexstr_to_bytes(connection["node_id"])
         return response["connections"]

tests/core/test_full_node_rpc.py
@@ -9,7 +9,7 @@ from chaingreen.full_node.signage_point import SignagePoint
 from chaingreen.protocols import full_node_protocol
 from chaingreen.rpc.full_node_rpc_api import FullNodeRpcApi
 from chaingreen.rpc.full_node_rpc_client import FullNodeRpcClient
-from chaingreen.rpc.rpc_server import start_rpc_server
+from chaingreen.rpc.rpc_server import NodeType, start_rpc_server
 from chaingreen.simulator.simulator_protocol import FarmNewBlockProtocol
 from chaingreen.types.spend_bundle import SpendBundle
 from chaingreen.types.unfinished_block import UnfinishedBlock
@@ -192,7 +192,9 @@ class TestRpc:
 
             await time_out_assert(10, num_connections, 1)
             connections = await client.get_connections()
-
+            assert NodeType(connections[0]["type"]) == NodeType.FULL_NODE.value
+            assert len(await client.get_connections(NodeType.FULL_NODE)) == 1
+            assert len(await client.get_connections(NodeType.FARMER)) == 0
             await client.close_connection(connections[0]["node_id"])
             await time_out_assert(10, num_connections, 0)
         finally: