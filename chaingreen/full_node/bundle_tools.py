kevinforsterch/chia-blockchain

Rename coin solution to coin spend (#6841)
* Rename `CoinSolution` as `CoinSpend` everywhere.

* Run `black` everywhere.

chia/full_node/bundle_tools.py
@@ -6,21 +6,21 @@ from clvm_tools import binutils
 
 from chia.full_node.generator import create_compressed_generator
 from chia.types.blockchain_format.program import SerializedProgram, Program
-from chia.types.coin_solution import CoinSolution
+from chia.types.coin_spend import CoinSpend
 from chia.types.generator_types import BlockGenerator, CompressorArg
 from chia.types.spend_bundle import SpendBundle
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.ints import uint32, uint64
 
 
-def spend_bundle_to_serialized_coin_solution_entry_list(bundle: SpendBundle) -> bytes:
+def spend_bundle_to_serialized_coin_spend_entry_list(bundle: SpendBundle) -> bytes:
     r = b""
-    for coin_solution in bundle.coin_solutions:
+    for coin_spend in bundle.coin_spends:
         r += b"\xff"
-        r += b"\xff" + SExp.to(coin_solution.coin.parent_coin_info).as_bin()
-        r += b"\xff" + bytes(coin_solution.puzzle_reveal)
-        r += b"\xff" + SExp.to(coin_solution.coin.amount).as_bin()
-        r += b"\xff" + bytes(coin_solution.solution)
+        r += b"\xff" + SExp.to(coin_spend.coin.parent_coin_info).as_bin()
+        r += b"\xff" + bytes(coin_spend.puzzle_reveal)
+        r += b"\xff" + SExp.to(coin_spend.coin.amount).as_bin()
+        r += b"\xff" + bytes(coin_spend.solution)
         r += b"\x80"
     r += b"\x80"
     return r
@@ -30,7 +30,7 @@ def simple_solution_generator(bundle: SpendBundle) -> BlockGenerator:
     """
     Simply quotes the solutions we know.
     """
-    cse_list = spend_bundle_to_serialized_coin_solution_entry_list(bundle)
+    cse_list = spend_bundle_to_serialized_coin_spend_entry_list(bundle)
     block_program = b"\xff"
 
     block_program += SExp.to(binutils.assemble("#q")).as_bin()
@@ -72,11 +72,11 @@ def compress_cse_puzzle(puzzle: SerializedProgram) -> Optional[bytes]:
     return match_standard_transaction_exactly_and_return_pubkey(puzzle)
 
 
-def compress_coin_solution(coin_solution: CoinSolution):
-    compressed_puzzle = compress_cse_puzzle(coin_solution.puzzle_reveal)
+def compress_coin_spend(coin_spend: CoinSpend):
+    compressed_puzzle = compress_cse_puzzle(coin_spend.puzzle_reveal)
     return [
-        [coin_solution.coin.parent_coin_info, coin_solution.coin.amount],
-        [compressed_puzzle, Program.from_bytes(bytes(coin_solution.solution))],
+        [coin_spend.coin.parent_coin_info, coin_spend.coin.amount],
+        [compressed_puzzle, Program.from_bytes(bytes(coin_spend.solution))],
     ]
 
 
@@ -86,20 +86,20 @@ def puzzle_suitable_for_compression(puzzle: SerializedProgram) -> bool:
 
 def bundle_suitable_for_compression(bundle: SpendBundle):
     ok = []
-    for coin_solution in bundle.coin_solutions:
-        ok.append(puzzle_suitable_for_compression(coin_solution.puzzle_reveal))
+    for coin_spend in bundle.coin_spends:
+        ok.append(puzzle_suitable_for_compression(coin_spend.puzzle_reveal))
     return all(ok)
 
 
-def compressed_coin_solution_entry_list(bundle: SpendBundle) -> List:
+def compressed_coin_spend_entry_list(bundle: SpendBundle) -> List:
     compressed_cse_list: List[List[Union[List[uint64], List[Union[bytes, None, Program]]]]] = []
-    for coin_solution in bundle.coin_solutions:
-        compressed_cse_list.append(compress_coin_solution(coin_solution))
+    for coin_spend in bundle.coin_spends:
+        compressed_cse_list.append(compress_coin_spend(coin_spend))
     return compressed_cse_list
 
 
 def compressed_spend_bundle_solution(original_generator_params: CompressorArg, bundle: SpendBundle) -> BlockGenerator:
-    compressed_cse_list = compressed_coin_solution_entry_list(bundle)
+    compressed_cse_list = compressed_coin_spend_entry_list(bundle)
     return create_compressed_generator(original_generator_params, compressed_cse_list)
 
 

chia/rpc/full_node_rpc_api.py
@@ -7,7 +7,7 @@ from chia.full_node.mempool_check_conditions import get_puzzle_and_solution_for_
 from chia.types.blockchain_format.program import Program, SerializedProgram
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.coin_record import CoinRecord
-from chia.types.coin_solution import CoinSolution
+from chia.types.coin_spend import CoinSpend
 from chia.types.full_block import FullBlock
 from chia.types.generator_types import BlockGenerator
 from chia.types.mempool_inclusion_status import MempoolInclusionStatus
@@ -511,7 +511,7 @@ class FullNodeRpcApi:
 
         puzzle_ser: SerializedProgram = SerializedProgram.from_program(Program.to(puzzle))
         solution_ser: SerializedProgram = SerializedProgram.from_program(Program.to(solution))
-        return {"coin_solution": CoinSolution(coin_record.coin, puzzle_ser, solution_ser)}
+        return {"coin_solution": CoinSpend(coin_record.coin, puzzle_ser, solution_ser)}
 
     async def get_additions_and_removals(self, request: Dict) -> Optional[Dict]:
         if "header_hash" not in request:

chia/wallet/trade_manager.py
@@ -11,7 +11,7 @@ from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.spend_bundle import SpendBundle
-from chia.types.coin_solution import CoinSolution
+from chia.types.coin_spend import CoinSpend
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.db_wrapper import DBWrapper
 from chia.util.hash import std_hash
@@ -360,14 +360,14 @@ class TradeManager:
         if trade_offer is not None:
             offer_spend_bundle: SpendBundle = trade_offer.spend_bundle
 
-        coinsols: List[CoinSolution] = []  # [] of CoinSolutions
-        cc_coinsol_outamounts: Dict[bytes32, List[Tuple[CoinSolution, int]]] = dict()
+        coinsols: List[CoinSpend] = []  # [] of CoinSpends
+        cc_coinsol_outamounts: Dict[bytes32, List[Tuple[CoinSpend, int]]] = dict()
         aggsig = offer_spend_bundle.aggregated_signature
         cc_discrepancies: Dict[bytes32, int] = dict()
         chia_discrepancy = None
         wallets: Dict[bytes32, Any] = dict()  # colour to wallet dict
 
-        for coinsol in offer_spend_bundle.coin_solutions:
+        for coinsol in offer_spend_bundle.coin_spends:
             puzzle: Program = Program.from_bytes(bytes(coinsol.puzzle_reveal))
             solution: Program = Program.from_bytes(bytes(coinsol.solution))
 
@@ -414,7 +414,7 @@ class TradeManager:
             )
             if chia_spend_bundle is not None:
                 for coinsol in coinsols:
-                    chia_spend_bundle.coin_solutions.append(coinsol)
+                    chia_spend_bundle.coin_spends.append(coinsol)
 
         zero_spend_list: List[SpendBundle] = []
         spend_bundle = None

tests/wallet_tools.py
@@ -8,7 +8,7 @@ from chia.types.announcement import Announcement
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.types.coin_solution import CoinSolution
+from chia.types.coin_spend import CoinSpend
 from chia.types.condition_opcodes import ConditionOpcode
 from chia.types.condition_with_args import ConditionWithArgs
 from chia.types.spend_bundle import SpendBundle
@@ -98,7 +98,7 @@ class WalletTool:
         fee: int = 0,
         secret_key: Optional[PrivateKey] = None,
         additional_outputs: Optional[List[Tuple[bytes32, int]]] = None,
-    ) -> List[CoinSolution]:
+    ) -> List[CoinSpend]:
         spends = []
 
         spend_value = sum([c.amount for c in coins])
@@ -143,32 +143,32 @@ class WalletTool:
                     ConditionWithArgs(ConditionOpcode.ASSERT_COIN_ANNOUNCEMENT, [primary_announcement_hash])
                 )
                 main_solution = self.make_solution(condition_dic)
-                spends.append(CoinSolution(coin, puzzle, main_solution))
+                spends.append(CoinSpend(coin, puzzle, main_solution))
             else:
-                spends.append(CoinSolution(coin, puzzle, self.make_solution(secondary_coins_cond_dic)))
+                spends.append(CoinSpend(coin, puzzle, self.make_solution(secondary_coins_cond_dic)))
         return spends
 
-    def sign_transaction(self, coin_solutions: List[CoinSolution]) -> SpendBundle:
+    def sign_transaction(self, coin_spends: List[CoinSpend]) -> SpendBundle:
         signatures = []
         solution: Program
         puzzle: Program
-        for coin_solution in coin_solutions:  # type: ignore # noqa
-            secret_key = self.get_private_key_for_puzzle_hash(coin_solution.coin.puzzle_hash)
+        for coin_spend in coin_spends:  # type: ignore # noqa
+            secret_key = self.get_private_key_for_puzzle_hash(coin_spend.coin.puzzle_hash)
             synthetic_secret_key = calculate_synthetic_secret_key(secret_key, DEFAULT_HIDDEN_PUZZLE_HASH)
             err, con, cost = conditions_for_solution(
-                coin_solution.puzzle_reveal, coin_solution.solution, self.constants.MAX_BLOCK_COST_CLVM
+                coin_spend.puzzle_reveal, coin_spend.solution, self.constants.MAX_BLOCK_COST_CLVM
             )
             if not con:
                 raise ValueError(err)
             conditions_dict = conditions_by_opcode(con)
 
             for _, msg in pkm_pairs_for_conditions_dict(
-                conditions_dict, bytes(coin_solution.coin.name()), self.constants.AGG_SIG_ME_ADDITIONAL_DATA
+                conditions_dict, bytes(coin_spend.coin.name()), self.constants.AGG_SIG_ME_ADDITIONAL_DATA
             ):
                 signature = AugSchemeMPL.sign(synthetic_secret_key, msg)
                 signatures.append(signature)
         aggsig = AugSchemeMPL.aggregate(signatures)
-        spend_bundle = SpendBundle(coin_solutions, aggsig)
+        spend_bundle = SpendBundle(coin_spends, aggsig)
         return spend_bundle
 
     def generate_signed_transaction(

ChainGreenOrg/chaingreen-blockchain

(cherry picked from commit 6cf29102f95b410a8c6dc416e612c998bac567fa)

chaingreen/full_node/bundle_tools.py
@@ -6,21 +6,21 @@ from clvm_tools import binutils
 
 from chaingreen.full_node.generator import create_compressed_generator
 from chaingreen.types.blockchain_format.program import SerializedProgram, Program
-from chaingreen.types.coin_solution import CoinSolution
+from chaingreen.types.coin_spend import CoinSpend
 from chaingreen.types.generator_types import BlockGenerator, CompressorArg
 from chaingreen.types.spend_bundle import SpendBundle
 from chaingreen.util.byte_types import hexstr_to_bytes
 from chaingreen.util.ints import uint32, uint64
 
 
-def spend_bundle_to_serialized_coin_solution_entry_list(bundle: SpendBundle) -> bytes:
+def spend_bundle_to_serialized_coin_spend_entry_list(bundle: SpendBundle) -> bytes:
     r = b""
-    for coin_solution in bundle.coin_solutions:
+    for coin_spend in bundle.coin_spends:
         r += b"\xff"
-        r += b"\xff" + SExp.to(coin_solution.coin.parent_coin_info).as_bin()
-        r += b"\xff" + bytes(coin_solution.puzzle_reveal)
-        r += b"\xff" + SExp.to(coin_solution.coin.amount).as_bin()
-        r += b"\xff" + bytes(coin_solution.solution)
+        r += b"\xff" + SExp.to(coin_spend.coin.parent_coin_info).as_bin()
+        r += b"\xff" + bytes(coin_spend.puzzle_reveal)
+        r += b"\xff" + SExp.to(coin_spend.coin.amount).as_bin()
+        r += b"\xff" + bytes(coin_spend.solution)
         r += b"\x80"
     r += b"\x80"
     return r
@@ -30,7 +30,7 @@ def simple_solution_generator(bundle: SpendBundle) -> BlockGenerator:
     """
     Simply quotes the solutions we know.
     """
-    cse_list = spend_bundle_to_serialized_coin_solution_entry_list(bundle)
+    cse_list = spend_bundle_to_serialized_coin_spend_entry_list(bundle)
     block_program = b"\xff"
 
     block_program += SExp.to(binutils.assemble("#q")).as_bin()
@@ -72,11 +72,11 @@ def compress_cse_puzzle(puzzle: SerializedProgram) -> Optional[bytes]:
     return match_standard_transaction_exactly_and_return_pubkey(puzzle)
 
 
-def compress_coin_solution(coin_solution: CoinSolution):
-    compressed_puzzle = compress_cse_puzzle(coin_solution.puzzle_reveal)
+def compress_coin_spend(coin_spend: CoinSpend):
+    compressed_puzzle = compress_cse_puzzle(coin_spend.puzzle_reveal)
     return [
-        [coin_solution.coin.parent_coin_info, coin_solution.coin.amount],
-        [compressed_puzzle, Program.from_bytes(bytes(coin_solution.solution))],
+        [coin_spend.coin.parent_coin_info, coin_spend.coin.amount],
+        [compressed_puzzle, Program.from_bytes(bytes(coin_spend.solution))],
     ]
 
 
@@ -86,20 +86,20 @@ def puzzle_suitable_for_compression(puzzle: SerializedProgram) -> bool:
 
 def bundle_suitable_for_compression(bundle: SpendBundle):
     ok = []
-    for coin_solution in bundle.coin_solutions:
-        ok.append(puzzle_suitable_for_compression(coin_solution.puzzle_reveal))
+    for coin_spend in bundle.coin_spends:
+        ok.append(puzzle_suitable_for_compression(coin_spend.puzzle_reveal))
     return all(ok)
 
 
-def compressed_coin_solution_entry_list(bundle: SpendBundle) -> List:
+def compressed_coin_spend_entry_list(bundle: SpendBundle) -> List:
     compressed_cse_list: List[List[Union[List[uint64], List[Union[bytes, None, Program]]]]] = []
-    for coin_solution in bundle.coin_solutions:
-        compressed_cse_list.append(compress_coin_solution(coin_solution))
+    for coin_spend in bundle.coin_spends:
+        compressed_cse_list.append(compress_coin_spend(coin_spend))
     return compressed_cse_list
 
 
 def compressed_spend_bundle_solution(original_generator_params: CompressorArg, bundle: SpendBundle) -> BlockGenerator:
-    compressed_cse_list = compressed_coin_solution_entry_list(bundle)
+    compressed_cse_list = compressed_coin_spend_entry_list(bundle)
     return create_compressed_generator(original_generator_params, compressed_cse_list)
 
 

chaingreen/rpc/full_node_rpc_api.py
@@ -7,7 +7,7 @@ from chaingreen.full_node.mempool_check_conditions import get_puzzle_and_solutio
 from chaingreen.types.blockchain_format.program import Program, SerializedProgram
 from chaingreen.types.blockchain_format.sized_bytes import bytes32
 from chaingreen.types.coin_record import CoinRecord
-from chaingreen.types.coin_solution import CoinSolution
+from chaingreen.types.coin_spend import CoinSpend
 from chaingreen.types.full_block import FullBlock
 from chaingreen.types.generator_types import BlockGenerator
 from chaingreen.types.mempool_inclusion_status import MempoolInclusionStatus
@@ -511,7 +511,7 @@ class FullNodeRpcApi:
 
         puzzle_ser: SerializedProgram = SerializedProgram.from_program(Program.to(puzzle))
         solution_ser: SerializedProgram = SerializedProgram.from_program(Program.to(solution))
-        return {"coin_solution": CoinSolution(coin_record.coin, puzzle_ser, solution_ser)}
+        return {"coin_solution": CoinSpend(coin_record.coin, puzzle_ser, solution_ser)}
 
     async def get_additions_and_removals(self, request: Dict) -> Optional[Dict]:
         if "header_hash" not in request:

chaingreen/wallet/trade_manager.py
@@ -11,7 +11,7 @@ from chaingreen.types.blockchain_format.coin import Coin
 from chaingreen.types.blockchain_format.program import Program
 from chaingreen.types.blockchain_format.sized_bytes import bytes32
 from chaingreen.types.spend_bundle import SpendBundle
-from chaingreen.types.coin_solution import CoinSolution
+from chaingreen.types.coin_spend import CoinSpend
 from chaingreen.util.byte_types import hexstr_to_bytes
 from chaingreen.util.db_wrapper import DBWrapper
 from chaingreen.util.hash import std_hash
@@ -360,14 +360,14 @@ class TradeManager:
         if trade_offer is not None:
             offer_spend_bundle: SpendBundle = trade_offer.spend_bundle
 
-        coinsols: List[CoinSolution] = []  # [] of CoinSolutions
-        cc_coinsol_outamounts: Dict[bytes32, List[Tuple[CoinSolution, int]]] = dict()
+        coinsols: List[CoinSpend] = []  # [] of CoinSpends
+        cc_coinsol_outamounts: Dict[bytes32, List[Tuple[CoinSpend, int]]] = dict()
         aggsig = offer_spend_bundle.aggregated_signature
         cc_discrepancies: Dict[bytes32, int] = dict()
         chaingreen_discrepancy = None
         wallets: Dict[bytes32, Any] = dict()  # colour to wallet dict
 
-        for coinsol in offer_spend_bundle.coin_solutions:
+        for coinsol in offer_spend_bundle.coin_spends:
             puzzle: Program = Program.from_bytes(bytes(coinsol.puzzle_reveal))
             solution: Program = Program.from_bytes(bytes(coinsol.solution))
 
@@ -420,7 +420,7 @@ class TradeManager:
             )
             if chaingreen_spend_bundle is not None:
                 for coinsol in coinsols:
-                    chaingreen_spend_bundle.coin_solutions.append(coinsol)
+                    chia_spend_bundle.coin_spends.append(coinsol)
 
         zero_spend_list: List[SpendBundle] = []
         spend_bundle = None

tests/wallet_tools.py
@@ -8,7 +8,7 @@ from chaingreen.types.announcement import Announcement
 from chaingreen.types.blockchain_format.coin import Coin
 from chaingreen.types.blockchain_format.program import Program
 from chaingreen.types.blockchain_format.sized_bytes import bytes32
-from chaingreen.types.coin_solution import CoinSolution
+from chaingreen.types.coin_spend import CoinSpend
 from chaingreen.types.condition_opcodes import ConditionOpcode
 from chaingreen.types.condition_with_args import ConditionWithArgs
 from chaingreen.types.spend_bundle import SpendBundle
@@ -98,7 +98,7 @@ class WalletTool:
         fee: int = 0,
         secret_key: Optional[PrivateKey] = None,
         additional_outputs: Optional[List[Tuple[bytes32, int]]] = None,
-    ) -> List[CoinSolution]:
+    ) -> List[CoinSpend]:
         spends = []
 
         spend_value = sum([c.amount for c in coins])
@@ -143,32 +143,32 @@ class WalletTool:
                     ConditionWithArgs(ConditionOpcode.ASSERT_COIN_ANNOUNCEMENT, [primary_announcement_hash])
                 )
                 main_solution = self.make_solution(condition_dic)
-                spends.append(CoinSolution(coin, puzzle, main_solution))
+                spends.append(CoinSpend(coin, puzzle, main_solution))
             else:
-                spends.append(CoinSolution(coin, puzzle, self.make_solution(secondary_coins_cond_dic)))
+                spends.append(CoinSpend(coin, puzzle, self.make_solution(secondary_coins_cond_dic)))
         return spends
 
-    def sign_transaction(self, coin_solutions: List[CoinSolution]) -> SpendBundle:
+    def sign_transaction(self, coin_spends: List[CoinSpend]) -> SpendBundle:
         signatures = []
         solution: Program
         puzzle: Program
-        for coin_solution in coin_solutions:  # type: ignore # noqa
-            secret_key = self.get_private_key_for_puzzle_hash(coin_solution.coin.puzzle_hash)
+        for coin_spend in coin_spends:  # type: ignore # noqa
+            secret_key = self.get_private_key_for_puzzle_hash(coin_spend.coin.puzzle_hash)
             synthetic_secret_key = calculate_synthetic_secret_key(secret_key, DEFAULT_HIDDEN_PUZZLE_HASH)
             err, con, cost = conditions_for_solution(
-                coin_solution.puzzle_reveal, coin_solution.solution, self.constants.MAX_BLOCK_COST_CLVM
+                coin_spend.puzzle_reveal, coin_spend.solution, self.constants.MAX_BLOCK_COST_CLVM
             )
             if not con:
                 raise ValueError(err)
             conditions_dict = conditions_by_opcode(con)
 
             for _, msg in pkm_pairs_for_conditions_dict(
-                conditions_dict, bytes(coin_solution.coin.name()), self.constants.AGG_SIG_ME_ADDITIONAL_DATA
+                conditions_dict, bytes(coin_spend.coin.name()), self.constants.AGG_SIG_ME_ADDITIONAL_DATA
             ):
                 signature = AugSchemeMPL.sign(synthetic_secret_key, msg)
                 signatures.append(signature)
         aggsig = AugSchemeMPL.aggregate(signatures)
-        spend_bundle = SpendBundle(coin_solutions, aggsig)
+        spend_bundle = SpendBundle(coin_spends, aggsig)
         return spend_bundle
 
     def generate_signed_transaction(